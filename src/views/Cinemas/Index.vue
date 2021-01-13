ded0054cffeb5b6f28a6548f11f807fceff96112
clear <870537430@qq.com>
2021-01-13 22:15:08 +0800

    over

--- src/views/Cinemas/Index.vue
+++ src/views/Cinemas/Index.vue
@@ -1,15 +1,78 @@
 <template>
+  <div class="header">
+    <div class="top">搜索</div>
     <div>
-        <div class="box">假装有页面</div>
+      <input
+        type="text"
+        placeholder="请输入要搜索的内容"
+        @input="searchAction"
+      />
+      <ul v-if="searchResult.length > 0">
+        <li v-for="(item, index) of searchResult" :key="index">
+          <div>
+            <img :src="item.goods_small_logo" :alt="item.goods_name" />
+          </div>
+          <h1>{{ item.goods_name }}</h1>
+        </li>
+      </ul>
+      <ul v-else v-show="false">
+        <li v-for="(item, index) of courseData" :key="index">
+          <div>
+            <img :src="item.goods_small_logo" :alt="item.goods_name" />
+          </div>
+          <h1>{{ item.goods_name }}</h1>
+        </li>
+      </ul>
     </div>
+    <div></div>
+  </div>
 </template>
+<script>
+import courseData from "@/data/course.js";
+export default {
+  name: "APP",
+  data() {
+    return {
+      searchResult: [],
+      courseData,
+    };
+  },
+  methods: {
+    searchAction(e) {
+      const inputValue = e.target.value;
+
+      this.searchResult = this.courseData.filter((item) => {
+        if (item.goods_name.includes(inputValue)) 
+        return item;
+      });
+    },
+  },
+};
+</script>
 <style scoped>
-    .box{
-        font-size:24px ;
-        text-align: center;
-        vertical-align: center;
-        font-weight: 700;
-        color: var(--color-tint);
-        line-height: 100px;
-    }
+.header {
+  text-align: center;
+  width: 100%;
+  font-size: 20px;
+  line-height: 40px;
+}
+.top {
+  width: 100%;
+  height: 40px;
+  background-color: var(--color-tint);
+  color: white;
+}
+input {
+  margin: 30px 10px 0 0;
+  width: 250px;
+  height: 40px;
+}
+.submit {
+  display: inline-block;
+  color: white;
+  font-weight: 400;
+  background-color: green;
+  height: 45px;
+  width: 40px;
+}
 </style>
